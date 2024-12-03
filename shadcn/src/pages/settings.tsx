82faa48f8761cbb5f23eccead705cc746085f93c
dillon-todd-dev <dillontodd.dev@gmail.com>
2024-12-03 13:14:12 -0600

    remove shadcn

--- shadcn/src/pages/settings.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from '@/components/ui/button';
-import { logout } from '@/lib/api';
-
-const Settings = () => {
-  return <Button onClick={() => logout()}>Logout</Button>;
-};
-
-export default Settings;
